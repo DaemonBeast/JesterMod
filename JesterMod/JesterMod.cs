8a8bef85a66112f9d3a4639a50cf6aa4977689af
DaemonBeast <DaemonBeast@outlook.com>
2021-02-19 12:21:51 +0000

    In the beginning, there was nothing...

---
+++ JesterMod/JesterMod.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using BepInEx;
+using BepInEx.IL2CPP;
+using HarmonyLib;
+using Reactor;
+
+namespace JesterMod
+{
+    [BepInPlugin(Id, Name, Version)]
+    [BepInProcess("Among Us.exe")]
+    [BepInDependency(ReactorPlugin.Id)]
+    public class JesterPlugin : BasePlugin
+    {
+        public const string Id = "daemon.jester.reactor";
+        public const string Name = "Jester Mod";
+        public const string Version = "0.1.0";
+
+        public Harmony Harmony { get; } = new Harmony(Id);
+
+        public override void Load()
+        {
+            Harmony.PatchAll();
+        }
+    }
+}
